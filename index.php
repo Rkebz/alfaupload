74c0b083d02ee4df541de21fa2a0ceecbc61babd
Rkebz <gfjhbttnd@gmail.com>
2025-05-12 17:55:50 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Hacked by Team Kuwaiti Shadow Force</title>
+  <style>
+    body {
+      background-color: white;
+      color: black;
+      text-align: center;
+      font-family: "Courier New", monospace;
+      margin: 0;
+      padding: 0;
+    }
+    .flag {
+      margin-top: 40px;
+      width: 300px; /*  */
+    }
+    .main {
+      margin-top: 30px;
+    }
+    h1 {
+      font-size: 30px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    .line {
+      font-size: 18px;
+      margin: 10px 0;
+    }
+    .marquee {
+      margin-top: 50px;
+      font-weight: bold;
+      font-size: 16px;
+      color: black;
+    }
+  </style>
+</head>
+<body>
+
+  <img class="flag" src="https://e.top4top.io/p_3414a174i0.jpeg" alt="Pakistan Flag">
+
+  <div class="main">
+    <h1>Hacked By Mr.RootVerse</h1>
+    <div class="line">
+      Greetings from to Mr.RootVerse the State of Pakistan. We are support you.
+    </div>
+    <div class="line">
+      Dear admin, your site is easy to hack.
+    </div>
+  </div>
+
+  <marquee class="marquee" behavior="scroll" direction="left">
+    Hacked By - Mr.RootVerse - Lulzsec Arabs - alfa
+  </marquee>
+
+</body>
+</html>
